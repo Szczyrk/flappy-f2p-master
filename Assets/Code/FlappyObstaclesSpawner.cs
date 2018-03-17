61bb879812ebc81bff96e06c4d349d13192f43eb
Szczyrk <jakubszczyrk@wp.pl>
2018-03-17 13:03:21 +0100

    kuba

---
+++ Assets/Code/FlappyObstaclesSpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+public class FlappyObstaclesSpawner : MonoBehaviour {
+
+	public GameObject obstaclePrefab;
+    public float spawnWait;
+    public float startWait;
+    public float x, y, gapHeight;
+    public float gapWidth;
+    public FlappyPlayer Flappy;   
+
+
+
+    List<GameObject> spawnedObstacles = new List<GameObject>();
+
+   void Start()
+    {
+       StartCoroutine(waitSpawner());
+    }
+
+    void Update()
+    {
+     
+    }
+   public void Spawn(float x, float y, float gapHeight)
+    {
+        
+        GameObject spawned = GameObject.Instantiate(obstaclePrefab);
+        spawned.transform.parent = transform;
+        spawned.transform.position = new Vector3(x+ Random.Range(-gapWidth, gapWidth), Random.Range (-y,y),0);
+        spawnedObstacles.Add(spawned);
+     
+        Transform bottomTransform = spawned.transform.FindChild("Bottom");
+        Transform topTransform = spawned.transform.FindChild("Top");
+        float bottomY = -Random.Range(gapHeight, gapHeight+3) / 2;
+        float topY = +Random.Range(gapHeight, gapHeight + 3) / 2;
+        bottomTransform.localPosition = Vector3.up * bottomY ;
+        topTransform.localPosition = Vector3.up * topY ;
+        Destroy(spawned, 10);
+        spawnedObstacles.RemoveAt(0);  
+       
+
+    }
+
+    public IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (Flappy.spown)
+            {
+                x += 10;
+                Spawn(x, y, gapHeight);
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+     
+    }
+}
---
+++ Assets/Code/FlappyPlayer.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent( typeof( Rigidbody ))]
+public class FlappyPlayer : MonoBehaviour {
+
+    public float movingSpeed = 5;
+    public float jumpForce = 10;
+    public float additionalGravityForce = 5;
+    public float rotationPercentage = 0.5f;
+	public float assumeDeadBelowY = -20;
+    public AudioSource Audio;
+    public AudioClip Hit;
+    public AudioClip Dead;
+    public float Wait = 1f;
+
+    public float waitDurationBeforeRestart = 3f;
+
+    public Rigidbody body {
+        get;
+        private set;
+	}
+
+	public bool IsDead() {
+		return state == State.DEAD;
+	}
+
+    public bool inputAllowed;
+    public bool spown;
+
+    public enum State {
+        INTRO, MOVING, DEAD
+    }
+    State state;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+        state = State.INTRO;
+        inputAllowed = true;
+    }
+
+    void Update() {
+        if ( inputAllowed && state == State.MOVING ) {
+			// Input update has to execute during Update, while logic can run in FixedUpdate
+            UpdateInput();
+            spown = true;
+        }
+    }
+
+    void FixedUpdate() {
+        switch ( state ) {
+            case State.INTRO:
+                UpdateIntro();
+                break;
+            case State.MOVING:
+                UpdateMoving();
+                ApplyAdditionalGravity();
+                break;
+            case State.DEAD:
+                ApplyAdditionalGravity();
+                break;
+        }
+    }
+
+    public bool Tapped() {
+        return Input.GetMouseButtonDown( 0 );
+    }
+
+    void UpdateIntro() {
+		// Constant velocity until first tap
+		body.velocity = Vector3.zero;
+        if ( Tapped() ) {
+            state = State.MOVING;
+            body.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+            GameObject.Find( "IntroText" ).SetActive( false );
+        }
+    }
+
+    void UpdateMoving() {
+		// Physics controls only Y axis of velocity
+        body.velocity = new Vector3( movingSpeed, body.velocity.y, 0 );
+		// Additional effect inspired by Flappy Bird - rotation based on velocity
+        transform.forward = Vector3.Lerp( Vector3.right, body.velocity, rotationPercentage );
+
+		if ( transform.position.y < assumeDeadBelowY ) {
+			Die();
+		}
+    }
+
+    void UpdateInput() {
+        if ( Tapped() ) {
+            Jump();
+        }
+    }
+
+    void Jump() {
+        body.velocity = new Vector3( movingSpeed, 0, 0 );
+        body.AddForce( Vector3.up * jumpForce, ForceMode.Impulse );
+    }
+
+    public void HitSound(AudioClip tmp)
+    {
+        Audio.PlayOneShot(tmp);
+    }
+    void OnCollisionEnter( Collision collision ) {
+        HitSound(Hit);
+        
+        Die();
+    }
+    IEnumerator Waitforhit()
+    {
+        yield return new WaitForSeconds(Wait);
+        HitSound(Dead);
+    }
+        void Die() {
+        StartCoroutine("Waitforhit");
+
+        inputAllowed = false;
+		state = State.DEAD;
+		body.constraints = RigidbodyConstraints.None;
+		StartCoroutine( RestartAfterSeconds( waitDurationBeforeRestart ) );
+       
+    }
+
+    void ApplyAdditionalGravity() {
+        body.AddForce( Vector3.down * additionalGravityForce );
+    }
+
+	IEnumerator RestartAfterSeconds( float seconds ) {
+		yield return new WaitForSeconds( seconds );
+		SceneManager.LoadScene( 0 ); // load first scene
+	}
+}
